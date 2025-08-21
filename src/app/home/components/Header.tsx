b099ace8f9f71445a753b5cb209831d4412ef3a2
fyvel <fdelage8000@gmail.com>
2025-08-22 08:26:09 +1000

    feat(css): Header

--- src/app/home/components/Header.tsx
+++ src/app/home/components/Header.tsx
@@ -3,19 +3,18 @@ import styles from "./header.module.css";
 export default function Header() {
   return (
     <header className={styles.hero}>
-      <div className={`${styles.container} ${styles.heroContent}`}>
-        <div className={`${styles.heroText} ${styles.spacing}`}>
-          <h1 className={styles["primary-title"]}>
+      <div className={`container ${styles.heroContent}`}>
+        <div className={`spacing`}>
+          <h1 className={styles.heroTitle}>
             Revolutionising Endometriosis Diagnosis
           </h1>
-          <p>Empowering women and healthcare professionals with fast, non-invasive, and insightful solutions.</p>
-					<p>Because women have waited long enough for better answers.</p>
-					<button>Call To Action</button>
+          <p className={styles.heroSubTitle}>
+            <mark>Empowering women and healthcare professionals with fast, non-invasive, and insightful solutions</mark>.
+          </p>
+          <p className={styles.heroQuote}><i>Because women have waited long enough for better answers.</i></p>
         </div>
-				<div className={styles.heroImage}>
-					<img src="https://images.unsplash.com/photo-1740205732463-8daff9ff0e57?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHx8MTc1NTI5MTY5OXww&ixlib=rb-4.1.0&q=80&w=1080" alt="Hero Image" />
-					</div>
+        <button className={`btn ${styles.ctaButton}`}>Call To Action</button>
       </div>
-    </header>
+    </header >
   );
 }
