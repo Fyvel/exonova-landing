eb0de1547b03b7995d66c89b7209617a8ea0d127
fyvel <fdelage8000@gmail.com>
2025-08-23 08:44:08 +1000

    feat(section): add `Problem.tsx`

--- src/app/home/components/Problem.tsx
+++ src/app/home/components/Problem.tsx
@@ -1,27 +1,52 @@
+import styles from './problem.module.css';
+
 export default function Problem() {
   return (
-    <section>
-      <h2>Endometriosis Diagnosis is Broken</h2>
-      <p>Current methods are invasive, costly, and unreliable.</p>
-      <p>Millions suffer in silence due to outdated diagnostic approaches.</p>
-      <ul>
-        <li>
-          6-12 years
-          Average wait for diagnosis
-        </li>
-        <li>
-          1 in 3
-          Women experience severe pelvic pain
-        </li>
-        <li>
-          10%
-          Of women have endometriosis
-        </li>
-        <li>
-          $8.9B AUD
-          Australian economic burden
-        </li>
-      </ul>
+    <section className={`section`}>
+      <div className={`container spacing`}>
+        <div className={`spacing`}>
+          <h2>Endometriosis Diagnosis is Broken</h2>
+          <p>
+            Current methods are invasive, costly, and unreliable.<br />
+            Millions suffer in silence due to outdated diagnostic approaches.
+          </p>
+        </div>
+        <ul className={styles.list}>
+          <Card
+            title="6 to 12 years"
+            description="Average wait for diagnosis"
+            src="/time.svg"
+          />
+          <Card
+            title="1 in 10"
+            description="Women have endometriosis"
+            src="/1-in-10.svg"
+          />
+          <Card
+            title="1 in 3"
+            description="Women experience severe pelvic pain"
+            src="/1-in-3.svg"
+          />
+          <Card
+            title="8.9B AUD"
+            description="Australian economic burden"
+            src="/money.svg"
+          />
+        </ul>
+      </div>
     </section>
   );
 }
+
+type CardProps = {
+  title: string;
+  description: string;
+  src: string;
+}
+const Card = ({ title, description, src }: CardProps) => (
+  <div className={styles.card}>
+    <img className={styles.image} src={src} alt={title} />
+    <h3 className={styles.title}>{title}</h3>
+    <p className={styles.description}>{description}</p>
+  </div>
+)
